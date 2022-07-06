447d20b5a2e19351ca5beb4171750cea17c6e213
andreimelo <andreiagno.melo@gmail.com>
2022-07-06 13:19:38 +0400

    setup: redux reducers and store

---
+++ src/library/common/config/store/index.js
@@ -0,0 +1,7 @@
+import { createStoreHook } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../../reducers ';
+
+const store = createStoreHook(rootReducer, composeWithDevTools());
+
+export default store;
